cb8f38fb319fa48f470f403aacb3fc42967fa684
Ha Nguyen <hank2@topica.edu.vn>
2020-02-12 23:45:57 +1100

    update

--- about/git.js
+++ about/git.js
@@ -3,7 +3,7 @@ const showoff = document.getElementById('ShowoffConner');
 const repo_icon = '<svg class="octicon octicon-repo mr-2 text-gray flex-shrink-0" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>'
 const fork_icon = '<svg aria-label="fork" class="octicon octicon-repo-forked" viewBox="0 0 10 16" version="1.1" width="10" height="16" role="img"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 00-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 002 1a1.993 1.993 0 00-1 3.72V6.5l3 3v1.78A1.993 1.993 0 005 15a1.993 1.993 0 001-3.72V9.5l3-3V4.72A1.993 1.993 0 008 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>'
 const star_icon = '<svg aria-label="stars" class="octicon octicon-star" viewBox="0 0 14 16" version="1.1" width="14" height="16" role="img"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>'
-var sorted_repos;
+
 
 String.prototype.format = function () {
     var i = 0, args = arguments;
@@ -19,10 +19,13 @@ function sortByKey(array, key) {
 }
 
 function getGitInfo() {
+    var sorted_repos;
     $.get("https://api.github.com/users/habom2310/repos", function(data, status){
         sorted_repos = sortByKey(data, "stargazers_count");
         console.log(sorted_repos);
       });
+
+    return sorted_repos;
 }
 
 function templateRepo(name, url, desc, stargazers_url, stargazers_count, forks_url, fork_count, language) {
@@ -54,7 +57,7 @@ function templateRepo(name, url, desc, stargazers_url, stargazers_count, forks_u
 }
 
 function fetchRepos() {
-    getGitInfo();
+    var sorted_repos = getGitInfo();
     console.log(sorted_repos);
     var repos_html = "";
     for (var i in sorted_repos){
